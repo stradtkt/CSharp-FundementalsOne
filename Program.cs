c086c9ed3b04a2d0b79f0b7039beb07d6eb9cddf
Kevin <stradtkt22@gmail.com>
2018-08-07 14:34:12 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FundementalsOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a loop that prints all values from 1-255
+            // for(int x = 1; x <= 255; x++)
+            // {
+            //     Console.WriteLine(x);
+            // }
+            Console.WriteLine("--------------------------------------");
+
+            //Create a new loop that prints all values from 1-100 that are divisible by 3 or 5, but not both
+            // for (int y = 1; y <= 100; y++)
+            // {
+            //      if (y % 15 == 0)
+            //     {
+            //         continue;
+            //     }
+            //     else if (y % 3 == 0 || y % 5 == 0)
+            //     {
+            //         Console.WriteLine(y);
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine('x');
+            //     }
+            // }
+            Console.WriteLine("--------------------------------------");
+
+            // for (int y = 1; y <= 100; y++)
+            // {
+            //      if (y % 15 == 0)
+            //     {
+            //         continue;
+            //     }
+            //     else if (y % 3 == 0)
+            //     {
+            //         Console.WriteLine("Fizz");
+            //     }
+            //     else if (y % 5 == 0)
+            //     {
+            //         Console.WriteLine("Buzz");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine(y);
+            //     }
+            // }
+            Console.WriteLine("----------------------------------------");
+
+            
+        }
+    }
+}
